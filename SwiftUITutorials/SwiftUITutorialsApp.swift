28d364d0f32c5ebdf7e8fda25a08e7a7ef307a66
Oguzhan <yoguzhnn@gmail.com>
2022-02-16 22:22:54 +0300

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Oguzhan Yılmaz on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

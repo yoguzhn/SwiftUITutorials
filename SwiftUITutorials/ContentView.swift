46c5c15ff4ef01099ba86aab47d649ddf0e7d2ca
Oguzhan <yoguzhnn@gmail.com>
2022-02-18 00:24:38 +0300

    Views samples added

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        sampleStackView()
     }
+    
+    fileprivate func sampleHorizontalView() -> some View {
+        return HStack {
+            Spacer().frame(width: 10, alignment: .center)
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Color.red
+            Spacer().frame(width: 10, alignment: .center)
+        }
+    }
+    
+    fileprivate func sampleVerticalView() -> some View {
+        return VStack {
+            Spacer().frame(width: 100, alignment: .center)
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+        }
+    }
+    fileprivate func sampleStackView() -> some View {
+        return ZStack {
+            Spacer().frame(width: 100, alignment: .center)
+            Color.red
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Text("Oguzhan!")
+                .padding()
+            Spacer().frame(width: 100, alignment: .center)
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
